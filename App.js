06ce46b32fc0ccccacf1d3ded582919fe0ae523e
AndjDavis <andi.idavis@gmail.com>
2020-05-11 22:27:39 -0600

    Landing renders on App.js

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
+import { globalStyles } from './src/common'
+import LandingScreen from './src/scenes/Landing'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={globalStyles.example}>
+      <Text style={globalStyles.exampleText}>Open up App.js to start working on your app!</Text>
+      <LandingScreen />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
